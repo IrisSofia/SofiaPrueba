a07e389937ff8b0fcedda06c68c2a4286c3d0fdf
Sofia <sofia@sofia-VirtualBox.(none)>
2013-02-20 04:29:47 -0600

    Agregando archivos

---
+++ src/Probando.java
@@ -0,0 +1,34 @@
+import java.io.*;
+public class Probando {
+
+private String nombre;
+private String provincia;
+private int poblacion;
+
+
+public String getNombre(){
+	return nombre;
+	}
+
+public void setNombre(String nombre){
+	
+	this.nombre=nombre;
+}
+public String getProvincia(){
+	return provincia;	
+}
+
+public void setProvincia(String provincia){
+	
+this.provincia=provincia;	
+}
+
+public int getPoblacion(){
+	return this.poblacion;
+	
+}
+
+public void setPoblacion(int poblacion){
+this.poblacion=poblacion;	
+}
+}
